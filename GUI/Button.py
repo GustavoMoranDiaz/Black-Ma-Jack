53026d0d553b132f4aa34cc0aa6d16b35a19bab5
GustavoMoranDiaz <gmorandi@stevens.edu>
2025-04-02 15:00:12 -0400

    created button class because i got annoyed working on just copying the buttons

---
+++ GUI/Button.py
@@ -0,0 +1,60 @@
+import pygame
+import numpy as np
+class Button:
+    def __init__(self,screen,XShift,YShift,buttonSize,buttonColor,buttonText,SCREEN_WIDTH,SCREEN_HEIGHT):
+
+        self.screen = screen
+        self.buttonSurface = pygame.surface(buttonSize)
+        self.buttonXShift = XShift
+        self.buttonYShift = YShift
+        self.buttonSize = buttonSize
+        self.buttonColor = buttonColor
+        self.buttonText = buttonText
+        self.buttonTextSurface = self.font.render(self.buttonText, True, (10, 10, 10))
+        self.SCREEN_WIDTH = SCREEN_WIDTH
+        self.SCREEN_HEIGHT = SCREEN_HEIGHT
+        
+        self.buttonRect = self.buttonSurface.get_rect(center=(SCREEN_WIDTH/2 + self.buttonYShift,(SCREEN_HEIGHT/2)+self.buttonYShift))
+        self.buttonTextRect = self.buttonTextSurface.get_rect(center=(SCREEN_WIDTH/2 + self.buttonYShift,(SCREEN_HEIGHT/2)+self.buttonYShift))
+
+    def DecoButton(self, cardFilePath):
+        if pygame.mouse.get_pos()[0] in range(self.buttonRect.left,self.buttonRect.right) and pygame.mouse.get_pos()[1] in range(self.buttonRect.top,self.buttonRect.bottom): #check to see if mouse is hovering the button
+            self.buttonSurface = pygame.transform.smoothscale(self.buttonSurface, ((self.buttonSize[0]*1.1,self.buttonSize[1]*1.1))) #make the button bigger if youre hovering it to indicate it is selected
+            self.buttonRect = self.buttonSurface.get_rect(center=(self.SCREEN_WIDTH/2 +self.buttonXShift,(self.SCREEN_HEIGHT/2)+self.buttonYShift)) #move button into position
+            self.buttonSurface.fill(self.buttonColor) #color the button
+            self.buttonTextSurface = self.font.render(self.buttonText, True, (10, 10, 10)) #create surface for the text on the button
+            self.buttonTextRect = self.buttonText.get_rect(center=(self.SCREEN_WIDTH/2 + self.buttonXShift,(self.SCREEN_HEIGHT/2)+self.buttonYShift)) # move the text into position
+
+            self.screen.blit(self.buttonSurface, self.buttonRect) # paste button onto screen
+            self.screen.blit(self.buttonTextSurface, self.buttonTextRect) # paste text onto screen
+            # start of Code for decorative rotating cards
+            self.buttonDecoSurface = pygame.image.load(cardFilePath).convert_alpha() #documentation states that using .convert() after loading a surface dramatically increases speed
+            self.buttonDecoSurface = pygame.transform.rotozoom(self.buttonDecoSurface, 4*np.cos(pygame.time.get_ticks()/300),0.15)
+            self.buttonDecoRect0 = self.buttonDecoSurface.get_rect(center=(self.buttonRect.left,self.SCREEN_HEIGHT/2 +self.buttonYShift))
+            self.buttonDecoRect1 = self.buttonDecoSurface.get_rect(center=(self.buttonRect.right,self.SCREEN_HEIGHT/2 +self.buttonYShift))
+
+            self.screen.blit(self.buttonDecoSurf,self.buttonDecoRect0)
+            self.screen.blit(self.buttonDecoSurf,self.buttonDecoRect1)
+            # end of Code for decorative rotating cards
+            # start of code for click detection and "screen change"
+            self.checkMouseUp = pygame.event.wait(15)
+            if self.checkMouseUp.type == pygame.MOUSEBUTTONUP:
+                # INSERT CODE TO OPEN THE GAME ONCE THATS DEVELOPED
+                print("button clicked") # delete this once you get the actual code for the game
+            # end of code for click detection and "screen change"
+        else:
+            self.buttonSurface = pygame.transform.smoothscale(self.buttonSurface, ((self.buttonSize[0],self.buttonSize[1]))) # make the button the original size when not hovering
+            self.buttonRect = self.buttonSurface.get_rect(center=(self.SCREEN_WIDTH/2 + self.buttonXShift,(self.SCREEN_HEIGHT/2)+self.buttonYShift)) #move button into position                self.buttonSurface.fill((133, 198, 255)) # color the button
+            self.buttonTextSurface = self.font.render(self.buttonText, True, (10, 10, 10)) # create surface for the text on the button
+            self.buttonTextRect = self.buttonText.get_rect(center=(self.SCREEN_WIDTH/2 + self.buttonXShift,(self.SCREEN_HEIGHT/2)+self.buttonYShift)) # move the text into position
+
+            self.screen.blit(self.buttonSurface, self.buttonRect) # paste button onto screen
+            self.screen.blit(self.buttonTextSurface, self.buttonTextRect) # paste text onto screen
+            # start of Code for decorative static cards
+            self.buttonDecoSurface = pygame.image.load(cardFilePath).convert_alpha() #documentation states that using .convert() after loading a surface dramatically increases speed
+            self.buttonDecoSurface = pygame.transform.rotozoom(self.buttonDecoSurface, 0,0.15)
+            self.buttonDecoRect0 = self.buttonDecoSurface.get_rect(center=(self.buttonRect.left,self.SCREEN_HEIGHT/2 +self.buttonYShift))
+            self.buttonDecoRect1 = self.buttonDecoSurface.get_rect(center=(self.buttonRect.right,self.SCREEN_HEIGHT/2 +self.buttonYShift))
+
+            self.screen.blit(self.buttonDecoSurface,self.buttonDecoRect0)
+            self.screen.blit(self.buttonDecoSurface,self.buttonDecoRect1)
