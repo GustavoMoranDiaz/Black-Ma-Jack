dd9e1298d96492de3545db729c5d12088046f38e
GustavoMoranDiaz <gmorandi@stevens.edu>
2025-04-29 14:07:03 -0400

    parameterized some of the tests to save on space and added a test for the stand function

--- test_CSVBlackJack.py
+++ test_CSVBlackJack.py
@@ -22,13 +22,13 @@ class TestCSVBlackJack:
 
     #--test of calculateHand Method--
 
-    def test_calculateHand1(self): # standard hand with no aces
-        result = self.deck.calculateHand([("7","H"),("J","S"),("4","D")])
-        assert result == 21
+    #test1 = 7 of hearts, jack of spades, 4 of diamonds should equal a score of 21
+    #test2 = ace of hearts, 2 of spades, king of spades should equal a score of 13 to avoid busting
 
-    def test_calculateHand2(self): # hand with aces, should, ideally go to the highest possible score without busting i.e 13
-        result = self.deck.calculateHand([("A","H"),("2","S"),("K","S")])
-        assert result == 13
+    @pytest.mark.parametrize("hand,expected",[([("7","H"),("J","S"),("4","D")],21),([("A","H"),("2","S"),("K","S")],13)])
+    def test_calculateHand(self, hand, expected): # standard hand with no aces
+        result = self.deck.calculateHand(hand)
+        assert result == expected
 
     #--test of buildHands--
 
@@ -43,4 +43,14 @@ class TestCSVBlackJack:
         hand1, hand2 = self.deck.buildHands(hand1,hand2) #create the hands
 
         assert list(hand1[0]) and list(hand1[1]) and list(hand2[0]) and list(hand2[1]) in top4cards # make sure that the hands are only composed of cards from the top 4 cards of the deck
-        
+
+    #--test of stand--
+    
+    #test1: playerhand = ace of hearts, king of diamonds | dealerhand = king of spades, 9 of clubs should result in a blackjack win for player i.e bjwin
+    #test2: playerhand = ace of hearts, king of diamonds, 8 of diamonds | dealerhand = king of spades, 9 of clubs, equal score should result in a push
+
+    @pytest.mark.parametrize("playerhand,dealerhand,expected", [([("A","H"),("K","D")],[("K","S"),("9","C")],"bjWin"),([("A","H"),("K","D"),("8","D")],[("K","S"),("9","C")],"push")])
+    def test_stand(self,playerhand,dealerhand,expected):
+        self.deck.stand(playerhand,dealerhand)
+        result = self.deck.getResult()
+        assert result == expected
